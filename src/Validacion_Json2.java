88f68c6f3ad9643589934423b6f0ced24f940538
Patish <Patricio.17po.@gmail.com>
2022-11-14 17:21:18 -0600

    Se hizo cambios esteticos al codigo

--- src/Validacion_Json2.java
+++ src/Validacion_Json2.java
@@ -7,13 +7,10 @@ import net.jimblackler.jsonschemafriend.Validator;
 public class Validacion_Json2 {
   public static void main(String[] args) {
     try {
-      SchemaStore schemaStore = new SchemaStore(); // Initialize a SchemaStore.
-      // Load the schema.
+
+      SchemaStore schemaStore = new SchemaStore(); 
       Schema schema = schemaStore.loadSchema(Validacion_Json2.class.getResource("/schema.json"));
-
       Validator validator = new Validator();
-
-      // Will not throw an exception.
       validator.validate(schema, Validacion_Json2.class.getResourceAsStream("/listado.json"));
 
     } catch (SchemaException | IOException e) {
