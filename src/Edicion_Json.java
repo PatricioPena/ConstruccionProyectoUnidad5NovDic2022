c7f8935358bdebec088c91020b76f94323b0cd7a
Andypotter10 <aeortizpalacios@hotmail.com>
2022-11-24 20:32:15 -0600

    Se agregó la clase para modificar el JSON

---
+++ src/Edicion_Json.java
@@ -0,0 +1,52 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Edicion_Json
+{
+	public void EdicionJson()
+	{
+		String identificador = JOptionPane.showInputDialog(null, "Ingresa el id del empleado a modificar");
+		Lectura_Json lectura = new Lectura_Json();
+		lectura.leerJson();
+		ArrayList<Empleado> empleados = lectura.getEmpleados();		
+		JSONObject json = new JSONObject();
+		JSONArray jsonArrayEmpleados = new JSONArray();
+		JSONObject jsonEmployee = new JSONObject();
+		
+		for(Empleado empleado: empleados){
+			JSONObject empleadotemp = new JSONObject();
+			if(identificador.equals(empleado.getId())){
+				String firstName = JOptionPane.showInputDialog(null, "Ingresa el pimer nombre del empleado " + identificador);
+				String lastName = JOptionPane.showInputDialog(null, "Ingresa el apellido del empleado " + identificador);
+				String photo = JOptionPane.showInputDialog(null, "Ingresa el link de la imagen del empleado " + identificador);
+				empleadotemp.put("id", identificador);
+				empleadotemp.put("firstName", firstName);
+				empleadotemp.put("lastName", lastName);
+				empleadotemp.put("photo", photo);			
+			}else{	
+				empleadotemp.put("id", empleado.getId());
+				empleadotemp.put("firstName", empleado.getFirstName());
+				empleadotemp.put("lastName", empleado.getLastName());
+				empleadotemp.put("photo", empleado.getPhoto());			
+
+			}
+			jsonArrayEmpleados.put(empleadotemp);
+		}
+
+		jsonEmployee.put("employee", jsonArrayEmpleados);
+		json.put("employees", jsonEmployee);
+
+		String path = "src/listado.json";
+		
+		try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
+			String jsonString = json.toString();
+			out.write(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
