a699e99eede22b04498681820ca165ae1263e119
Andypotter10 <aeortizpalacios@hotmail.com>
2022-11-24 20:38:40 -0600

    Se adapto la clase PantallaTest para poder ejecutar la modificacion del JSON

--- src/PantallaTest.java
+++ src/PantallaTest.java
@@ -11,9 +11,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.awt.image.BufferedImage;
 
-public class PantallaTest extends JFrame {
-    public PantallaTest() {
+public class PantallaTest extends JFrame implements ActionListener {
+    private JButton modificar;
+    private JButton agregar;
+    private JButton eliminar;
 
+    public PantallaTest() {
         super("Lectura del Json");
         try {
             // creamos el modelo de Tabla
@@ -22,7 +25,7 @@ public class PantallaTest extends JFrame {
                     return getValueAt(0, column).getClass();
                 }
             };
-            
+
             // se crea la Tabla con el modelo DefaultTableModel
             final JTable table = new JTable(dtm);
 
@@ -34,13 +37,13 @@ public class PantallaTest extends JFrame {
 
             // creamos un objeto para centrar la tabla
             DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-            centerRenderer.setHorizontalAlignment( JLabel.CENTER );
-            
+            centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+
             // insertamos el tamaño de las columnas y centramos los datos
             for (int i = 0; i < 3; i++) {
                 table.getColumnModel().getColumn(i).setPreferredWidth(50);
                 table.getColumnModel().getColumn(i).setResizable(false);
-                table.getColumnModel().getColumn(i).setCellRenderer( centerRenderer );
+                table.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
             }
             table.getColumnModel().getColumn(3).setPreferredWidth(200);
             table.getColumnModel().getColumn(3).setResizable(false);
@@ -55,12 +58,12 @@ public class PantallaTest extends JFrame {
                 Image image = getScaledImage(imageIcon.getImage(), 200, 150);
                 imageIcon = new ImageIcon(image);
                 Icon icon = (Icon) imageIcon;
-                dtm.addRow(new Object[] { empleado.getId(), empleado.getFirtsName(), empleado.getLastName(), icon });
+                dtm.addRow(new Object[] { empleado.getId(), empleado.getFirstName(), empleado.getLastName(), icon });
 
             }
 
             // se define el tamaño
-            table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+            table.setPreferredScrollableViewportSize(new Dimension(1500, 720));
 
             // Creamos un JscrollPane y le agregamos la JTable
             JScrollPane scrollPane = new JScrollPane(table);
@@ -74,6 +77,27 @@ public class PantallaTest extends JFrame {
                     System.exit(0);
                 }
             });
+
+            setLayout(new FlowLayout());
+
+            this.modificar = new JButton("Modificar");
+
+            this.agregar = new JButton("Agregar");
+
+            this.eliminar = new JButton("Eliminar");
+
+            add(this.modificar);
+            add(Box.createRigidArea(new Dimension(10, 0)));
+
+            add(this.agregar);
+            add(Box.createRigidArea(new Dimension(10, 0)));
+
+            add(this.eliminar);
+
+            this.modificar.addActionListener(this);
+
+            this.agregar.addActionListener(this);
+            this.eliminar.addActionListener(this);
 
         } catch (IOException e) {
 
@@ -89,5 +113,27 @@ public class PantallaTest extends JFrame {
         g2.dispose();
 
         return resizedImg;
+    }
+
+    public JButton getButtonModificar(JButton button) {
+        return button;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (this.modificar.equals(e.getSource())) {
+            Edicion_Json edicion = new Edicion_Json();
+            edicion.EdicionJson();
+            dispose();
+            Reload();
+        }
+    }
+
+    public void Reload(){
+        PantallaTest frame = new PantallaTest();
+        frame.pack();
+        frame.setBounds(0,0,1920,1280);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
